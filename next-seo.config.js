17f1c608146f0d09711e2c2a6e5a859a97d39289
Antonin Nhek <a.nhek@icloud.com>
2021-09-15 19:57:08 +0200

    added: next-seo, prop-types, real favicon. moved files to a src folder.

---
+++ next-seo.config.js
@@ -0,0 +1,66 @@
+const canonicalUrl = ''
+
+// Edit the default title
+const defaultTitle = 'Site title'
+
+// Edit the SEO parameters
+export const SEO = {
+  defaultTitle: defaultTitle,
+  titleTemplate: `${defaultTitle} | %s`,
+  canonical: canonicalUrl,
+  additionalLinkTags: [
+    {
+      rel: 'icon',
+      href: 'favicons/favicon-16x16.png',
+      type: 'image/png',
+      sizes: '16x16',
+    },
+    {
+      rel: 'icon',
+      href: 'favicons/favicon-32x32.png',
+      type: 'image/png',
+      sizes: '32x32',
+    },
+    {
+      rel: 'apple-touch-icon',
+      href: 'favicons/apple-touch-icon.png',
+      sizes: '180x180',
+    },
+    {
+      rel: 'manifest',
+      href: 'favicons/site.webmanifest',
+    },
+    {
+      rel: 'mask-icon',
+      href: 'favicons/safari-pinned-tab.svg',
+      // Edit the touch bar background color
+      color: '#81e6d9',
+    },
+  ],
+  additionalMetaTags: [
+    {
+      name: 'msapplication-TileColor',
+      // Edit the tile color (windows)
+      content: '#81e6d9',
+    },
+    {
+      name: 'theme-color',
+      // Edit the full background color (android)
+      content: '#ffffff',
+    },
+  ],
+  openGraph: {
+    title: defaultTitle,
+    type: 'website',
+    locale: 'en_IE',
+    url: canonicalUrl,
+  },
+  // Edit the twitter info or delete
+  twitter: {
+    handle: '@handle',
+    site: '@site',
+    cardType: 'summary_large_image',
+  },
+}
+
+export default SEO
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,13 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import { DefaultSeo } from 'next-seo'
 
+import SEO from '../../next-seo.config'
 import overrides from '../theme/index'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={overrides}>
+      <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </ChakraProvider>
   )
